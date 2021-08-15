fc1c8db613ebbfb3479cb4e216773e3c5906f762
Tora0615 <5438arthur@gmail.com>
2021-08-16 00:05:28 +0800

    feat : const, var and type infer(推斷) exercise.

--- Swift Exercise/Exercise01.playground/Contents.swift
+++ Swift Exercise/Exercise01.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+// 這是註解
+// 執行可以按數字上的執行按鈕，或是利用快捷鍵 shift + command + enter
 
-var greeting = "Hello, playground"
+/* 00 - Hello world */
+import UIKit
+var greeting = "Hello, playground"   // 結果會顯現在右邊
+
+/* 01 - 常數與變數*/
+
+// 變數宣告關鍵字是 var
+var x = 10
+
+// 常數宣告關鍵字是 let
+let constantNum = 20
+
+var y = x + constantNum
+
+/* 02 - 型別推論*/
+// swift 預設會從值推論其型別
+// 型別的種類有：Int、Double、String、Bool 等(注意：為大寫)
+// 格式為 -> 變數名稱：型別 = 值
+var num : Int = 10
+var floatNum : Double = 12.34
+var str : String = "字串"
+var boolvar : Bool = true
+// 自動推斷型別的變數，可以按住 option 並點選變數名稱來查看
+
+
